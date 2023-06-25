558153fd22332d1f1f2b0bf65346c0e05328fe4b
Minjae Chang <minjae.j.chang@gmail.com>
2023-06-25 19:43:16 +1200

    update the form to add item with delete button

--- main.js
+++ main.js
@@ -2,18 +2,27 @@ const list = document.querySelector('.list');
 const button = document.querySelector('.submitBtn');
 const form = document.querySelector('.form');
 
+let deleteBtns;
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const input = document.querySelector('.input').value;
+  const input = document.querySelector('.input');
   const item = document.createElement('li');
-  item.textContent = `${input}`;
-  list.appendChild(item);
-});
+  item.setAttribute('class', 'item');
 
-const deleteBtns = document.querySelectorAll('.deleteBtn');
-for (let button of deleteBtns) {
-  button.addEventListener('click', (event) => {
-    const item = button.parentNode;
+  const span = document.createElement('span');
+  span.textContent = input.value;
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = `<i class="fa-solid fa-trash"></i>`;
+  deleteBtn.addEventListener('click', () => {
+    const item = deleteBtn.parentNode;
     item.remove();
   });
-}
+
+  item.append(span);
+  item.append(deleteBtn);
+  list.append(item);
+
+  form.reset();
+});
