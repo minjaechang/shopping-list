fb0726d5ea461377a2cbbf701e2ff44aac9f16e0
Minjae Chang <minjae.j.chang@gmail.com>
2023-06-25 19:26:08 +1200

    Add delete function

--- main.js
+++ main.js
@@ -9,3 +9,11 @@ form.addEventListener('submit', (event) => {
   item.textContent = `${input}`;
   list.appendChild(item);
 });
+
+const deleteBtns = document.querySelectorAll('.deleteBtn');
+for (let button of deleteBtns) {
+  button.addEventListener('click', (event) => {
+    const item = button.parentNode;
+    item.remove();
+  });
+}
